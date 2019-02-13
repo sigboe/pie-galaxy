5e9b4d849f3c1680b344fa3fadaa24d2d0dfd776
sigurdb <sigurdb@redpill-linpro.com>
2019-02-13 19:23:23 +0100

    lint

--- pie-galaxy.sh
+++ pie-galaxy.sh
@@ -7,7 +7,6 @@ basename=$(basename "${0}")
 renderhtml="html2text"
 #version="0.1" #set a version when the core function work
 
-
 _depends() {
 	#wyvern needs cargo and libssl-dev
 	#wyvern needs $HOME/.cargo/bin in path
@@ -51,7 +50,7 @@ _ls() {
 
 	if [[ -n "${selectedGame}" ]]; then
 		gameName=$(echo "${wyvernls}" | jq --raw-output --argjson var "${selectedGame}" '.games[] | .ProductInfo | select(.id==$var) | .title')
-		gameDescription=$(curl -s "http://api.gog.com/products/${selectedGame}?expand=description" | jq --raw-output '.description | .full' | $renderhtml )
+		gameDescription=$(curl -s "http://api.gog.com/products/${selectedGame}?expand=description" | jq --raw-output '.description | .full' | $renderhtml)
 
 		dialog --backtitle "${title}" --title "${gameName}" --ok-label "Select" --msgbox "${gameDescription}" 22 77
 	fi
@@ -85,7 +84,7 @@ _down() {
 	_menu
 }
 
-_checklogin(){
+_checklogin() {
 	if [[ -f "${HOME}/.config/wyvern/wyvern.toml" ]]; then
 		if grep -q "access_token =" "${HOME}/.config/wyvern/wyvern.toml"; then
 			wyvernls=$(wyvern ls --json)
@@ -94,40 +93,39 @@ _checklogin(){
 			exit 1
 		fi
 	fi
-		# url=$(timeout 1 wyvern ls | head -2 | tail -1)
+	# url=$(timeout 1 wyvern ls | head -2 | tail -1)
 
-		# curl --cookie-jar cjar --output /dev/null "${url}"
+	# curl --cookie-jar cjar --output /dev/null "${url}"
 
-		# curl --cookie cjar --cookie-jar cjar \
-		# 	--data "login[username]=${goguser}" \
-		# 	--data "login[password]=${gogpass}" \
-		# 	--data "form_id=login" \
-		# 	--location \
-		# 	--output login-result.html \
-		# 	"${url}/login_check"
+	# curl --cookie cjar --cookie-jar cjar \
+	# 	--data "login[username]=${goguser}" \
+	# 	--data "login[password]=${gogpass}" \
+	# 	--data "form_id=login" \
+	# 	--location \
+	# 	--output login-result.html \
+	# 	"${url}/login_check"
 
+	#wyvern ls
 
-		#wyvern ls
-
-		#try something fancy here, want to open a terminal based webbrowser, and fetch the token from the URL name and pass it back to wyvern
+	#try something fancy here, want to open a terminal based webbrowser, and fetch the token from the URL name and pass it back to wyvern
 }
 
-_about(){
+_about() {
 	dialog --backtitle "${title}" --msgbox "This graphical user interface is made possible by Nico Hickman's Wyvern which is a terminal based GOG client. ${title} was developed to make make it useful on RetroPie." 22 77
 	#this about screen can get a bit more detailed
 	_menu
 }
 
-_sync(){
+_sync() {
 	dialog --backtitle "${title}" --msgbox "This feature is not written yet for RetroPie." 22 77
 	#need to write a sync, maybe open a menu to check for games with support or something.
 	_menu
 }
 
-_install(){
-    fileSelected=$(dialog --title "${title}" --stdout --fselect "${tmpdir}" 22 77)
+_install() {
+	fileSelected=$(dialog --title "${title}" --stdout --fselect "${tmpdir}" 22 77)
 
-    gameID=$(innoextract -s --gog-game-id "${fileSelected}")
+	gameID=$(innoextract -s --gog-game-id "${fileSelected}")
 	gameName=$(echo "${wyvernls}" | jq --raw-output --argjson var "${gameID}" '.games[] | .ProductInfo | select(.id==$var) | .title')
 
 	rm -rf "${tmpdir}/app" #clean the extract path (is this okay to do like this?)
@@ -145,13 +143,13 @@ _install(){
 		cd "${romdir}" || exit 1
 		ln -s "${basename%/*}/ScummVM-template.sh" "${gameName}.sh"
 	fi
-	
+
 	clear
 	echo "${fileSelected}" #this shouldn't be here when this function works.
 	#_menu
 }
 
-_getType(){
+_getType() {
 
 	_path=$(cat "${1}"/goggame-*.info | jq --raw-output '.playTasks[] | select(.isPrimary==true) | .path')
 
@@ -159,7 +157,7 @@ _getType(){
 		type="dosbox"
 	elif [[ "${_path}" == *"SCUMMVM"* ]]; then
 		# not tested
-		type="scummvm" 
+		type="scummvm"
 	elif [[ "${_path}" == *"neogeo"* ]]; then
 		# Surly this wont work, but its a placeholder
 		type="neogeo"
@@ -168,7 +166,7 @@ _getType(){
 		_menu
 		# can maybe detect and install some ports too.
 	fi
-	
+
 }
 
 _exit() {
