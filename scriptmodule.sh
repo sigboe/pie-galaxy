1fe6495794a701b34182a0025a5359dc57df91d1
sigurdb <sigurdb@redpill-linpro.com>
2019-02-24 13:27:57 +0100

    conveluted workaround for downloadAndExtract not passing arguments to tar.

--- scriptmodule.sh
+++ scriptmodule.sh
@@ -15,16 +15,31 @@ rp_module_licence="GPL https://github.com/sigboe/pie-galaxy/blob/master/LICENSE"
 rp_module_section="exp"
 
 function depends_piegalaxy() {
-    getDepends jq html2text unar
+	getDepends jq html2text unar
 }
 
-function install_bin_piegalaxy() {	
+function install_bin_piegalaxy() {
 	local innoversion="1.8-dev-2019-01-13"
 	gitPullOrClone "$md_inst" https://github.com/sigboe/pie-galaxy.git master
-	isPlatform "x86" && (cd "$md_inst" && curl -o wyvern -O https://demenses.net/wyvern-nightly)
-	isPlatform "arm" && (cd "$md_inst" && curl -o wyvern -O https://demenses.net/wyvern-1.3.0-armv7)
-	isPlatform "x86" && downloadAndExtract "http://constexpr.org/innoextract/files/snapshots/innoextract-${innoversion}/innoextract-${innoversion}-linux.tar.xz" "$md_inst" 3 innoextract-${innoversion}-linux/bin/amd64/innoextract
-	isPlatform "arm" && downloadAndExtract "http://constexpr.org/innoextract/files/snapshots/innoextract-${innoversion}/innoextract-${innoversion}-linux.tar.xz" "$md_inst" 3 innoextract-${innoversion}-linux/bin/armv6j-hardfloat/innoextract
+
+	if isPlatform "x86"; then
+		downloadAndExtract "http://constexpr.org/innoextract/files/snapshots/innoextract-${innoversion}/innoextract-${innoversion}-linux.tar.xz" "$md_inst/innotemp" 2
+		mv "$md_inst/innotemp/amd64/innoextract" "$md_inst/"
+		rm -rf "$md_inst/innotemp"
+		(
+			cd "$md_inst" || exit 1
+			curl -o wyvern -O https://demenses.net/wyvern-nightly
+		)
+	fi
+	if isPlatform "arm"; then
+		downloadAndExtract "http://constexpr.org/innoextract/files/snapshots/innoextract-${innoversion}/innoextract-${innoversion}-linux.tar.xz" "$md_inst/innotemp" 2
+		mv "$md_inst/innotemp/armv6j-hardfloat/innoextract" "$md_inst/"
+		rm -rf "$md_inst/innotemp"
+		(
+			cd "$md_inst" || exit 1
+			curl -o wyvern -O https://demenses.net/wyvern-1.3.0-armv7
+		)
+	fi
 	chmod +x "$md_inst"/wyvern "$md_inst"/innoextract "$md_inst"/pie-galaxy.sh
 }
 
