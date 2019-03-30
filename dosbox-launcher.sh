810ee980ed96d4d88475b05dd9537636a41f29d0
Sigurd Boe <sig.boe@gmail.com>
2019-03-30 14:05:06 +0100

    Try to fix launching of dosbox games installed with .sh installers

--- dosbox-launcher.sh
+++ dosbox-launcher.sh
@@ -5,6 +5,7 @@ game=$(basename -s .sh "${0}")
 
 # DOSBox settings override
 export DOSBOX_SDL_USESCANCODES=false
+export LC_ALL="C"
 
 if [[ -x "/opt/retropie/emulators/dosbox/bin/dosbox" ]]; then
 	emu="/opt/retropie/emulators/dosbox/bin/dosbox"
@@ -15,41 +16,49 @@ if ! [[ -x "$(command -v ${emu:-dosbox})" ]]; then
 	exit 1
 fi
 
-echo "Launching ${game}"
-cd "${dosboxromdir}/${game}/DOSBOX" || exit 1
+if [[ -d "${dosboxromdir}/${game}/DOSBOX" ]]; then
+	workdir="${dosboxromdir}/${game}/DOSBOX"
+	cd "${workdir}" || exit 1
 
-#fixes
-if [[ -f "../dosboxSTARGUN_single.conf" ]] && ! grep -q "pause" ../dosboxSTARGUN_single.conf; then
-	#This fixes stargunner from crashing on load on linux
-	ed -s ../dosboxSTARGUN_single.conf <<< $'g/STARGUN\\.exe/i\\\npause\nw'
-fi
+	#fixes
+	if [[ -f "../dosboxSTARGUN_single.conf" ]] && ! grep -q "pause" ../dosboxSTARGUN_single.conf; then
+		#This fixes stargunner from crashing on load on linux
+		ed -s ../dosboxSTARGUN_single.conf <<<$'g/STARGUN\\.exe/i\\\npause\nw'
+	fi
 
-dosboxargs=( $(jq --raw-output '.playTasks[] | select(.isPrimary==true) | .arguments' ../goggame-*.info | sed 's:\\:/:g;s:\"::g') )
+	readarray -t dosboxargs < <(jq --raw-output '.playTasks[] | select(.isPrimary==true) | .arguments' ../goggame-*.info | sed 's:\\:/:g;s:\"::g')
 
-if [[ -z "${dosboxargs[*]}" ]]; then
+	# If case there is no json file with the launch command, we guess the launch command
+	if [[ -z "${dosboxargs[*]}" ]]; then
+		dosboxargs=(
+			-conf
+			"$(find .. -maxdepth 1 -name 'dosbox*.conf' | awk '{print length " " $1}' | sort -n | head -1 | awk '{print $2}')"
+			-conf
+			"$(find .. -maxdepth 1 -name 'dosbox*single.conf' | head -1)"
+			-c
+			exit
+		)
+
+	fi
+
+elif [[ -d "${dosboxromdir}/${game}/dosbox" ]]; then
+	workdir="${dosboxromdir}/${game}"
+	cd "${workdir}" || exit 1
 
 	dosboxargs=(
 		-conf
-		"$(find .. -maxdepth 1 -name 'dosbox*.conf' | awk '{print length " " $1}' | sort -n | head -1 | awk '{print $2}')"
+		"$(find . -maxdepth 1 -name 'dosbox*.conf' | awk '{print length " " $1}' | sort -n | head -1 | awk '{print $2}')"
 		-conf
-		"$(find .. -maxdepth 1 -name 'dosbox*single.conf' | head -1)"
+		"$(find . -maxdepth 1 -name 'dosbox*single.conf' | head -1)"
 		-c
 		exit
 	)
 
+else
+	exit 1
 fi
 
-# match=$(echo "${dosboxargs[@]:0}" | grep -o "STARGUN") && if [[ -n "${match}" ]]; then
-# 	#pause emulation before game starts, because it will crash if not
-# 	printf '[autoexec]\npause' > dosboxSTARGUN_pause.conf
-# 	dosboxargs=(
-# 		"${dosboxargs[@]:0:2}"
-# 		-config
-# 		dosboxSTARGUN_pause.conf
-# 		"${dosboxargs[@]:2}"
-# 	)
-# fi
-
 echo "Found arugments: ${dosboxargs[*]}"
+echo "Launching ${game}"
 
-"${emu:-dosbox}" ${dosboxargs[@]}
+"${emu:-dosbox}" "${dosboxargs[@]}"
