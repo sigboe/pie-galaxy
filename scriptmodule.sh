29fe94d4c3c282fb2f75f1d6cc9649254af2df15
sigurdb <sigurdb@redpill-linpro.com>
2019-04-23 21:39:39 +0200

    update installer, please manually re add the installer

--- scriptmodule.sh
+++ scriptmodule.sh
@@ -22,7 +22,7 @@ function install_bin_piegalaxy() {
 	local innoversion="1.8-dev-2019-01-13"
 	gitPullOrClone "$md_inst" https://github.com/sigboe/pie-galaxy.git master
 	isPlatform "x86" && (cd "$md_inst" && curl -o wyvern -O https://demenses.net/wyvern-nightly)
-	isPlatform "arm" && (cd "$md_inst" && curl -o wyvern -O https://demenses.net/wyvern-1.3.0-armv7)
+	isPlatform "arm" && (cd "$md_inst" && curl -o wyvern -O https://demenses.net/wyvern-arm-nightly)
 	isPlatform "x86" && downloadAndExtract "http://constexpr.org/innoextract/files/snapshots/innoextract-${innoversion}/innoextract-${innoversion}-linux.tar.xz" "$md_inst" --strip-components 3 innoextract-${innoversion}-linux/bin/amd64/innoextract
 	isPlatform "arm" && downloadAndExtract "http://constexpr.org/innoextract/files/snapshots/innoextract-${innoversion}/innoextract-${innoversion}-linux.tar.xz" "$md_inst" --strip-components 3 innoextract-${innoversion}-linux/bin/armv6j-hardfloat/innoextract
 	chmod +x "$md_inst"/wyvern "$md_inst"/innoextract "$md_inst"/pie-galaxy.sh
